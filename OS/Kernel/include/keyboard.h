e312d222a5d72378870893eac99d73cbe9308df7
Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>
2020-05-27 12:46:59 -0300

    video incompleto commit arreglo

---
+++ OS/Kernel/include/keyboard.h
@@ -0,0 +1,8 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+void keyboard_handler();
+char getKey();
+char hasKey();
+char getPort64();
+
+#endif
---
+++ OS/Kernel/keyboard/keyboardasm.asm
@@ -0,0 +1,13 @@
+GLOBAL getKey
+GLOBAL hasKey
+GLOBAL getPort64
+getKey:
+    in al,60h
+    ret
+hasKey:
+    in al,64h
+    and al,1
+    ret
+getPort64:
+    in al,64h
+    ret
