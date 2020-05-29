eb0084131505d704abf2fa98d106b36ea34b9c5f
Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>
2020-05-29 17:28:39 -0300

    prueba de tamano

--- OS/Userland/UserlandCodeModule/Main/userlandCodeModule.c
+++ OS/Userland/UserlandCodeModule/Main/userlandCodeModule.c
@@ -1,9 +1,10 @@
 #include <syscalls.h>
 
 int main() {
-	sys_drawCharacter(0,0,10,'a');
-	sys_drawCharacter(11,0,20,'a');
-	sys_drawCharacter(22,0,30,'a');
-	sys_drawCharacter(51,0,40,'a');
+	int scale=1;
+	int offset=0;
+	while(scale<43){
+		sys_drawCharacter(offset+=scale,0,scale+=1,'a');
+	}
 	return 0;
 }
