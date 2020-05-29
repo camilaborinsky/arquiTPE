bb338a57b7c3ad0d5f8d51c3e5e2186327df9036
Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>
2020-05-29 17:19:56 -0300

    arreglo syscall draw

--- OS/Userland/UserlandCodeModule/Main/userlandCodeModule.c
+++ OS/Userland/UserlandCodeModule/Main/userlandCodeModule.c
@@ -1,6 +1,9 @@
 #include <syscalls.h>
 
 int main() {
-	sys_drawCharacter(0,0,41,'a');
+	sys_drawCharacter(0,0,10,'a');
+	sys_drawCharacter(11,0,20,'a');
+	sys_drawCharacter(22,0,30,'a');
+	sys_drawCharacter(51,0,40,'a');
 	return 0;
 }
--- OS/Userland/UserlandCodeModule/syscalls/syscallsasm.asm
+++ OS/Userland/UserlandCodeModule/syscalls/syscallsasm.asm
@@ -6,4 +6,7 @@ sys_drawCharacter: ;int x, int y, int px, char letter
     mov rbp,rsp
     mov rax, 1
     int 80h
+    mov rsp,rbp
+    pop rbp
+    ret
 
