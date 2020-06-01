019d4f246215d9c0c543bd5816ef6700d15e1d2a
atormakh <atormakh@itba.edu.ar>
2020-05-31 21:01:42 -0300

    (working) backspace

--- OS/Userland/UserlandCodeModule/windowHandling/windows.c
+++ OS/Userland/UserlandCodeModule/windowHandling/windows.c
@@ -2,15 +2,26 @@
 #include <stdlib.h>
 #include <syscalls.h>
 
-void printString(char * string,tabStruct * tab){
-    int width=tab->currentScreen.xf - tab->currentScreen.xi;
-    int px=tab->px;
-    int lettersPerLine = width/px; //cambiar a syscall getResolution
-    for(int i=0;string[i]!=0;i++){
-         int x_offset = tab->currentScreen.xi + px*((tab->current)%lettersPerLine);
-        int y_offset = tab->currentScreen.yi + (2*px)*((tab->current)/lettersPerLine);
-        sys_drawCharacter(x_offset,y_offset,px,string[i]);
-        tab->current++;
+void printString(char *string, tabStruct *tab)
+{
+    int width = tab->currentScreen.xf - tab->currentScreen.xi;
+    int px = tab->px;
+    int lettersPerLine = width / px; //cambiar a syscall getResolution
+    for (int i = 0; string[i] != 0; i++){
+        if (string[i] == 8){
+            if(tab->current != 0){
+                string[i] = 32;
+                tab->current--;
+                int x_offset = tab->currentScreen.xi + px * ((tab->current) % lettersPerLine);
+                int y_offset = tab->currentScreen.yi + (2 * px) * ((tab->current) / lettersPerLine);
+                sys_drawCharacter(x_offset, y_offset, px, string[i]);
+            }
+        }
+        else{
+            int x_offset = tab->currentScreen.xi + px * ((tab->current) % lettersPerLine);
+            int y_offset = tab->currentScreen.yi + (2 * px) * ((tab->current) / lettersPerLine);
+            sys_drawCharacter(x_offset, y_offset, px, string[i]);
+            tab->current++;
+        }
     }
-    
 }
